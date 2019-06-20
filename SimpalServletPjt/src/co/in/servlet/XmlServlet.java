cb0a7026585301a533121592e8aeeed423704496
wr0ngway <yogichoudhary9715@gmail.com>
2019-06-20 20:49:34 +0530

    Update

--- SimpalServletPjt/src/co/in/servlet/XmlServlet.java
+++ SimpalServletPjt/src/co/in/servlet/XmlServlet.java
@@ -17,22 +17,35 @@ public class XmlServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		String userName = request.getParameter("userName");
 		out.println("Hello from GET mathod!" + userName);
 	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		String userName = request.getParameter("userName");
 		String fullName = request.getParameter("fullName");
 		out.println("Hello from POST method!" + userName + "!we know your full name is" + fullName);
-	}
+		String prof = request.getParameter("prof");
+		out.println("You are a " + prof);
 
+		// String location = request.getParameter("location");
+		String[] location = request.getParameterValues("location");
+		out.println("You are at " + location.length + "places");
+		for (int i = 0; i < location.length; i++) {
+			out.println(location[i]);
+
+		}
+
+	}
 
 }
