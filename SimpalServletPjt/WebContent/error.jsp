c7a30adab64312a51e5ed5d1b22cf500c0cffd6a
wr0ngway <yogichoudhary9715@gmail.com>
2019-06-29 10:03:09 +0530

    first commit

---
+++ SimpalServletPjt/WebContent/error.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Unsuccessful Login Page</title>
+</head>
+<body bgcolor="red">
+ <center>
+  Unsuccessful Login
+ </center>
+</body>
+</html>
--- SimpalServletPjt/src/co/in/servlet/SimpleServlet.java
+++ SimpalServletPjt/src/co/in/servlet/SimpleServlet.java
@@ -8,16 +8,30 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @WebServlet(description = "A simple servlet", urlPatterns = { "/SimpleServlet" })
 public class SimpleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String SavedeUserName = null;
 
 		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Hello from GET method.");
-		PrintWriter writer = response.getWriter();
-		writer.println("<h3>Hello</h3>");
+			// Set response content type
+		      response.setContentType("text/html");
+
+		      PrintWriter write = response.getWriter();
+		      String userName =request.getParameter("name");
+		      HttpSession session = request.getSession();
+		      if (userName != "" && userName != null) {
+		    	  session.setAttribute("SavedUserName", userName);
+				
+			}
+		      session.setAttribute("SavedeUserName", userName);
+		      write.println("requset parameter has username as " + userName);
+		      write.println("Session parameter has username as  " + session.getAttribute(SavedeUserName));
+		      
+		      
 	}
 
 }
